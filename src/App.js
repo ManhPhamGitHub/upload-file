fb26ead1ad636a4e673f123f0709bf7cf0867f4a
Huy Hoang Nguyen <superdiro23@gmail.com>
2020-10-22 13:12:44 +0700

    fix render

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
 import Routes from './routes';
-import Menu from './components/common/Menu'
-import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Menu />
         <Routes />
       </div>
     );
--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,6 +1,6 @@
 import { all } from 'redux-saga/effects';
 
-function* rootSaga() {
+export default function* rootSaga() {
   yield all([
   ]);
 }
