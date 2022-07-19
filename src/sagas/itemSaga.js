6aa3ceb16781da95190296876aac46013b7f6a2d
ManhPhamGitHub <90790565+ManhPhamGitHub@users.noreply.github.com>
2022-07-19 22:53:51 +0700

    Update itemSaga.js

--- src/sagas/itemSaga.js
+++ src/sagas/itemSaga.js
@@ -13,7 +13,8 @@ function* getItemSaga(action) {
 function* addItemSaga(action) {
     console.log('213123213');
     console.log('dasd');
-
+    console.log('cc wibu');
+    console.log('yeuemhiennnn');
     try {
         let form = action.payload.form
         console.log(form,'res');
