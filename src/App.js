679c89b2410a89fd5b80f203a78ffd5b86df8d0a
annn52@wru.vn <Dhy181102>
2022-07-19 22:15:15 +0700

    test git app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import Routes from './routes';
+import Menu from './components/common/Menu'
+import './App.css'
 
 class App extends Component {
   render() {
+    console.log("dasdsadsadsadsadsa");
     return (
       <div className="App">
+        <Menu />
         <Routes />
       </div>
     );
